2bdb0ceba736e49e68884fe9c9c7decf28f747cb
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2026-02-27 14:09:26 +0700

    adding user Id to the model

--- src/models/MyTrips.js
+++ src/models/MyTrips.js
@@ -1,47 +1,53 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 const tripSchema = new Schema(
-  {
-    type: {
-        type: String,
-        required: true
-    },
-    name: {
-      type: String,
-      required: true,
-      unique: true
-    },
+    {
+        type: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true,
+        },
 
-    short_description: {
-      type: String,
-      required: true,
-    },
+        short_description: {
+            type: String,
+            required: true,
+        },
 
-    image_url: {
-      type: String,
-      required: true
-    },
-    location_name: {
-      type: String,
-    },
+        image_url: {
+            type: String,
+            required: true
+        },
+        location_name: {
+            type: String,
+        },
 
-    location: {
-      type: {
-        type: String,
-        enum: ["Point"],
-        default: "Point"
-      },
-      coordinates: {
-        type: [Number],
-      }
+        location: {
+            type: {
+                type: String,
+                enum: ["Point"],
+                default: "Point"
+            },
+            coordinates: {
+                type: [Number],
+            }
+        },
+
+        user: {
+            type: Types.ObjectId,
+            ref: "User",
+            required: true
+        }
+    },
+    {
+        timestamps: true
     }
-  },
-  {
-    timestamps: true
-  }
 );
 
 tripSchema.index({ location: "2dsphere" });
+tripSchema.index({ name: 1, user: 1 }, { unique: true });
 
 const Trip = model('Trip', tripSchema);
 
